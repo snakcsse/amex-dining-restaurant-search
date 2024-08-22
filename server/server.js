59c09297eab1a2efc45e42e0b8c75c564715b3cb
snakcsse <snakcsse@gmail.com>
2024-08-22 17:30:46 +0900

    check corsOptions

--- server/server.js
+++ server/server.js
@@ -10,6 +10,7 @@ process.on('uncaughtException', (err) => {
 });
 
 dotenv.config({ path: './config.env' });
+console.log('PROD_FRONTEND_URL:', process.env.PROD_FRONTEND_URL);
 
 //Connect to MongoDB using mongoose (need to run npm i mongoose)
 const DB = process.env.DATABASE.replace('<password>', process.env.DATABASE_PASSWORD);
