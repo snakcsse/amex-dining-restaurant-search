845a3420b3ae4210b024668f69ef402b1d16c58b
snakcsse <snakcsse@gmail.com>
2024-08-27 15:51:35 +0900

    Fix redis bug

--- server/controllers/restaurantController.js
+++ server/controllers/restaurantController.js
@@ -1,32 +1,32 @@
 const Restaurant = require('../models/restaurantModel');
-const redisClient = require('../config/redisClient');
+// const redisClient = require('../config/redisClient');
 
 exports.getAllRestaurants = async (req, res, next) => {
   try {
-    if (process.env.NODE_ENV === 'production') {
-      const cacheKey = 'all_restaurants';
+    // if (process.env.NODE_ENV === 'production') {
+    // const cacheKey = 'all_restaurants';
 
-      // check if data is in redis
-      const cachedRestaurants = await redisClient.get(cacheKey);
+    // check if data is in redis
+    // const cachedRestaurants = await redisClient.get(cacheKey);
 
-      if (cachedRestaurants) {
-        console.log('Cache hit');
-        return res
-          .status(200)
-          .json({ status: 'success', data: { restaurants: JSON.parse(cachedRestaurants) } });
-      }
+    // if (cachedRestaurants) {
+    // console.log('Cache hit');
+    // return res
+    // .status(200)
+    // .json({ status: 'success', data: { restaurants: JSON.parse(cachedRestaurants) } });
+    // }
 
-      console.log('Cache miss');
-    }
+    // console.log('Cache miss');
+    // }
 
     const restaurants = await Restaurant.find().select(
       '-address -googleMapPage -discountTimePeriod -discountExcludeDay -closeDay -lunchDiscount -area_original -__v'
     );
 
     // Store data in cache
-    if (process.env.NODE_ENV === 'production') {
-      await redisClient.setEx(cacheKey, 2592000, JSON.stringify(restaurants));
-    } // cache for 1 hour
+    // if (process.env.NODE_ENV === 'production') {
+    // await redisClient.setEx(cacheKey, 2592000, JSON.stringify(restaurants));
+    // } // cache for 1 hour
 
     res.status(200).json({
       status: 'success',
